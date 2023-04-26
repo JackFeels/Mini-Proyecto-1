b15f6507819cbfff6de065710a175d589bf5458c
JackFeels <107821314+JackFeels@users.noreply.github.com>
2023-04-26 00:52:39 -0400

    50% listo
    Tenemos find, insert right, print, size, insert left y funcionando como abstract class ListArr.h :)

---
+++ ListArray.cpp
@@ -0,0 +1,99 @@
+#include "ListArray.h"
+
+ListArray::ListArray(int capacity)
+{
+    head = new NodeArray(capacity);
+    head->size = 0;
+    head->capacity = capacity;
+    head->next = nullptr;
+}
+ListArray::~ListArray()
+{
+    NodeArray *nodo_actual = head;
+    while (nodo_actual != nullptr)
+    {
+        NodeArray *next_node = nodo_actual->next;
+        delete nodo_actual;
+        nodo_actual = next_node;
+    }
+}
+int ListArray::size()
+{
+    int total_size = 0;
+    NodeArray *nodo_actual = head;
+    while (nodo_actual != nullptr)
+    {
+        total_size += nodo_actual->size;
+        nodo_actual = nodo_actual->next;
+    }
+    return total_size;
+}
+void ListArray::insert_left(int v)
+{
+    if (head->size == head->capacity)
+    {
+        NodeArray *new_node = new NodeArray(head->capacity);
+        new_node->size = 0;
+        new_node->next = head;
+        head = new_node;
+    }
+    for (int i = head->size; i > 0; i--)
+    {
+        head->data[i] = head->data[i - 1];
+    }
+    head->data[0] = v;
+    head->size++;
+}
+void ListArray::print()
+{
+    NodeArray *nodo_actual = head;
+    while (nodo_actual != nullptr)
+    {
+        for (int i = 0; i < nodo_actual->size; i++)
+        {
+            std::cout << nodo_actual->data[i] << " ";
+        }
+        nodo_actual = nodo_actual->next;
+    }
+    std::cout << std::endl;
+}
+void ListArray::insert_right(int v) {
+    NodeArray*  nodo_actual= head;
+    while (nodo_actual->next != nullptr) {
+        nodo_actual = nodo_actual->next;
+    }
+    if (nodo_actual->size == nodo_actual->capacity) {
+        nodo_actual->next = new NodeArray(head->capacity);
+        nodo_actual = nodo_actual->next;
+        nodo_actual->size = 0;
+        nodo_actual->next = nullptr;
+    }
+    nodo_actual->data[nodo_actual->size] = v;
+    nodo_actual->size++;
+}
+void ListArray::insert(int v, int i)
+{
+    // Si la lista está vacía o i == size(), insertar al final
+    if (head == nullptr || i == size())
+    {
+        insert_right(v);
+    }
+}
+bool ListArray::find(int v)
+{
+    NodeArray *nodo_actual = head;
+    while (nodo_actual != nullptr)
+    {
+        for (int i = 0; i < nodo_actual->size; i++)
+        {
+            if (nodo_actual->data[i] == v)
+            {   
+                cout << "El elemento " << v << " se encuentra en el ListArr" << endl;
+                return true;
+            }
+        }
+        nodo_actual = nodo_actual->next;
+    }
+    cout << "El elemento " << v << " NO se encuentra en el ListArr" << endl;
+    return false;
+}
--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
-#include "ListArr.h"
+#include "ListArray.h"
 
 int main(){
     int capacidad; // tamaño arreglo b
     std:: cout << "Ingrese el tamaño del arreglo: ";
     std:: cin >> capacidad;
-    ListArr l_a(capacidad);
+    ListArray l_a(capacidad);
     l_a.insert_left(1);
     l_a.insert_left(2);
     l_a.insert_left(15);
     l_a.insert_left(13);
+    l_a.insert_left(69);
+    l_a.insert_left(11);
+    l_a.insert_left(13);
+    l_a.insert_right(8);
+    l_a.insert_right(444);
+    l_a.find(15);
+    l_a.find(100);
 
     cout << "size: " << l_a.size() << endl;
     l_a.print();
